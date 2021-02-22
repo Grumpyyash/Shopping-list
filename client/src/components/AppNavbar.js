e6dc667d5570b1ca9dca62487a6fdc51afa69598
Yash Vardhan <yashvardhan513@gmail.com>
2021-02-22 20:39:52 +0530

    Add client folder back to git repo

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends React.Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+       this.setState({
+           isOpen: !this.state.isOpen
+       });
+    }
+
+    render(){
+        return (
+          <div>
+              <Navbar color="dark" dark expand="sm" className="mb-5">
+                  <Container>
+                      <NavbarBrand href="/">Shopping list</NavbarBrand>
+                      <NavbarToggler onClick={this.toggle} />
+                      <Collapse isOpen={this.state.isOpen} navbar>
+                          <Nav className="ml-auto" navabr>
+                            <NavItem>
+                                <NavLink href="https://github.com/Grumpyyash">Github</NavLink>
+                            </NavItem>
+                          </Nav>
+                      </Collapse>
+                  </Container>
+              </Navbar>
+          </div>
+        );
+    }
+}
+export default AppNavbar;
